7f20f4944066c89f02104bf0c1cf82f0f862cae8
kiran <kbb1582001@gmail.com>
2023-09-09 13:28:00 +0530

    Make a api call to fetch video

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+const ChannelCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChannelCard
